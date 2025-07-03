17601aae4e0d7165ed4a47b07154f5d55d4a7839
MUHAMMADFARHAN1 <Farhan_75230@hotmail.com>
2025-07-03 15:12:55 +0200

    Update Interest.js

--- Backend/models/Interest.js
+++ Backend/models/Interest.js
@@ -24,7 +24,7 @@ const interestSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "Offer",
     },
-    from_user: {
+    user_interested: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
