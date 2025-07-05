66de929cda3b7e3b0888f307c6981e274192422d
Muhammad Farhan <farhan_75230@hotmail.com>
2025-07-05 12:59:26 +0200

    buttons created!

--- Frontend/src/components/sidebar.tsx
+++ Frontend/src/components/sidebar.tsx
@@ -3,7 +3,13 @@ import { Button } from "@/components/ui/button";
 
 function Sidebar() {
   return (
-    <div className=" py-4 w-full h-screen max-h-screen min-h-[500px] min-w-[190px] flex flex-column justify-between  bg-emerald-500"></div>
+    <div className=" py-4 w-full h-screen max-h-screen min-h-[500px] min-w-[190px] col-span-1 bg-emerald-500">
+      <div className="flex flex-col ml-2 w-20 gap-10 mt-5">
+        <Button>Books</Button>
+        <Button>Offers</Button>
+        <Button>Profile</Button>
+      </div>
+    </div>
   );
 }
 
