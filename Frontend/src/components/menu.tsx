e68b44962e2cc04517b83e43462e2dc6df902660
Muhammad Farhan <farhan_75230@hotmail.com>
2025-07-05 12:51:55 +0200

    redundnat colour removed!

--- Frontend/src/components/menu.tsx
+++ Frontend/src/components/menu.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 function menu() {
   return (
-    <div className=" py-4 max-w-screen min-w-[1500px] bg-[#f4f4f4] flex flex-row justify-between  bg-emerald-500">
+    <div className=" py-4 max-w-screen min-w-[1500px] flex flex-row justify-between  bg-emerald-500">
       <p className="ml-2 py-1 font-medium ">Buchtausch</p>
       <Button className="mr-2 bg-blue-600">Logout</Button>
     </div>
