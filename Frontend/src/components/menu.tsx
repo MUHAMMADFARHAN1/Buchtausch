65973dceadb343edec56bade131fc6dfe4e28073
Muhammad Farhan <farhan_75230@hotmail.com>
2025-07-05 12:31:12 +0200

    menu adjusted!

--- Frontend/src/components/menu.tsx
+++ Frontend/src/components/menu.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 function menu() {
   return (
     <div className=" py-4 max-w-screen bg-[#f4f4f4] flex flex-row justify-between">
-      <p className="ml-2 py-1 font-medium ">Challenges</p>
+      <p className="ml-2 py-1 font-medium ">Buchtausch</p>
       <Button className="mr-2 bg-blue-600">Logout</Button>
     </div>
   );
