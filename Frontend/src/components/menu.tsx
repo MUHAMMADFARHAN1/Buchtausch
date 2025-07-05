d0783ccf2e4037e8e4bf76a3d87a4eebeb06505f
Muhammad Farhan <farhan_75230@hotmail.com>
2025-07-05 13:02:50 +0200

    buttons created!

--- Frontend/src/components/menu.tsx
+++ Frontend/src/components/menu.tsx
@@ -5,7 +5,7 @@ function menu() {
   return (
     <div className=" py-4 max-w-screen min-w-[1500px] flex flex-row justify-between  bg-emerald-500">
       <p className="ml-2 py-1 font-medium ">Buchtausch</p>
-      <Button className="mr-2 bg-blue-600">Logout</Button>
+      <Button className="mr-2 bg-lime-600">Logout</Button>
     </div>
   );
 }
--- Frontend/src/components/sidebar.tsx
+++ Frontend/src/components/sidebar.tsx
@@ -4,10 +4,10 @@ import { Button } from "@/components/ui/button";
 function Sidebar() {
   return (
     <div className=" py-4 w-full h-screen max-h-screen min-h-[500px] min-w-[190px] col-span-1 bg-emerald-500">
-      <div className="flex flex-col ml-2 w-20 gap-10 mt-5">
-        <Button>Books</Button>
-        <Button>Offers</Button>
-        <Button>Profile</Button>
+      <div className="flex flex-col ml-2 w-20 gap-10 mt-1">
+        <Button className="bg-lime-600">Books</Button>
+        <Button className="bg-lime-600">Offers</Button>
+        <Button className="bg-lime-600">Profile</Button>
       </div>
     </div>
   );
