0598844efbe256fd31772deebacef9cf135f82b5
Muhammad Farhan <farhan_75230@hotmail.com>
2025-07-14 14:40:06 +0200

    finally fetch performed!

--- Frontend/src/app/api/data/route.ts
+++ Frontend/src/app/api/data/route.ts
@@ -3,14 +3,27 @@ import { NextResponse } from "next/server";
 import { cookies } from "next/headers"; // Server-side cookie access
 import { auth, signIn, signOut } from "@/auth";
 import { getToken } from "next-auth/jwt";
-
+import jwt from "jsonwebtoken";
+import jwt_decode from "jwt-decode";
 
 export async function GET() {
   let session: any = await auth();
   console.log(session.accessToken);
   let accessToken = session.accessToken;
-  //return session?.user;
-  return NextResponse.json({ accessToken });
+
+  const response = await fetch("http://127.0.0.1:5001/Books/", {
+    method: "GET",
+    headers: {
+      Authorization:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4NzIzYjkyN2Q5MWIzZGQxMzM3Y2EzNSIsImlhdCI6MTc1MjQ5MjI4OCwiZXhwIjoxNzUzMDk3MDg4fQ.jreLeoegO6BI8hKYBqy869lfK2oHxWRIK_0ffdFwawg",
+      //  Accept: "application/json",
+    },
+    mode: "cors",
+  });
+
+  const array = await response.json();
+  console.log(array);
+  return NextResponse.json({ response });
 
   //   const token = await getToken({
   //     req,
