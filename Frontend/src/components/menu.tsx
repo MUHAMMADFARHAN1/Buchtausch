a497aa70df64940747622312a474944ec52adaec
Muhammad Farhan <farhan_75230@hotmail.com>
2025-07-14 16:29:01 +0200

    Favicon adjusted

--- Frontend/src/components/menu.tsx
+++ Frontend/src/components/menu.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { logout } from "@/api/auth";
+import Image from "next/image";
+import favicon from "./../../../Assests/favicon_io/favicon12.png";
 
 function menu() {
   return (
     <div className=" py-4 max-w-screen min-w-[1500px] flex flex-row justify-between  bg-emerald-500">
-      <p className="ml-2 py-1 font-medium ">Buchtausch</p>
+      <p className="ml-8 py-1 font-medium ">
+        <Image src={favicon} className="m-auto" alt="" />
+      </p>
       <form action={logout}>
         <Button className="mr-2 bg-lime-600">Logout</Button>
       </form>
