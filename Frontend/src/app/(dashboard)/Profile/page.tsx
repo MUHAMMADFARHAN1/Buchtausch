a2b059847305a7bfb90d6c3234c4ab57636dcf3e
Muhammad Farhan <farhan_75230@hotmail.com>
2025-07-15 17:55:00 +0200

    Profile updated

--- Frontend/src/app/(dashboard)/Profile/page.tsx
+++ Frontend/src/app/(dashboard)/Profile/page.tsx
@@ -4,7 +4,18 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 function page() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState({
+    // _id: "68723b927d91b3dd1337ca35",
+    // name: "Muhammad Farhan",
+    // email: "farhan_75230@hotmail.com",
+    // city: "Coburg, Deutschland",
+    // phone: 1745277296,
+    // password: "$2b$10$3gy1BqpfcwnXtoiafNiCmO.7ER2qEA1n.DWcXBFJxnqGMuX2AvgV.",
+    // verified: false,
+    // createdAt: "2025-07-12T10:40:18.320Z",
+    // updatedAt: "2025-07-12T10:40:18.320Z",
+    // __v: 0,
+  });
   const [loading, setLoading] = useState(true); // Initially true, so "loading" screen shows
   const [error, setError] = useState(null);
 
@@ -15,38 +26,62 @@ function page() {
         return res.json();
       })
       .then((jsonData) => {
-        setData(jsonData);
-        let { city, email, name, phone } = jsonData[0];
         setLoading(false); // Fetch done, so set loading to false
+        // let { city: string, name, phone, email } = jsonData;
+        setData({
+          _id: "68723b927d91b3dd1337ca35",
+          name: "Muhammad Farhan",
+          email: "farhan_75230@hotmail.com",
+          city: "Coburg, Deutschland",
+          phone: 1745277296,
+          password:
+            "$2b$10$3gy1BqpfcwnXtoiafNiCmO.7ER2qEA1n.DWcXBFJxnqGMuX2AvgV.",
+          verified: false,
+          createdAt: "2025-07-12T10:40:18.320Z",
+          updatedAt: "2025-07-12T10:40:18.320Z",
+          __v: 7,
+        });
         console.log(jsonData);
-        console.log(city);
+        console.log(data);
+        console.log(loading);
+        console.log(data);
       })
       .catch((err) => {
         setError(err.message);
         setLoading(false);
       });
   }, []);
+  if (loading) {
+    // Render this while loading (this blocks showing the rest)
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <div className="grid grid-cols-2 mx-4">
       <div>
         {/* <img src="" alt="" /> */}
-        <p>hello</p>
+        <img
+          src="https://api.dicebear.com/9.x/avataaars/svg?seed=Farhan"
+          alt="Profile"
+          className="w-4/5"
+          onError={(e) => (e.currentTarget.style.display = "none")} // Hide if invalid URL/image
+        />
       </div>
       <div className="flex flex-col gap-10 mt-20">
         <label>
           Name:
-          <input
-            type="text"
-            name="username"
-            // value={baseData.username}
-            readOnly
-          />
+          <br />
+          <input type="text" name="username" value={data.name} readOnly />
         </label>
         <br />
 
         <label>
           Email:
+          <br />
           <input
             type="text"
             name="profileUrl"
@@ -58,6 +93,7 @@ function page() {
 
         <label>
           City:
+          <br />
           <input
             type="text"
             name="fullProfileUrl"
@@ -69,6 +105,7 @@ function page() {
 
         <label>
           Phone:
+          <br />
           <input
             type="text"
             name="fullProfileUrl"
