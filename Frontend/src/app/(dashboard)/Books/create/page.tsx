69178e9b768bc674e5e754e20b94ddd301600f12
Muhammad Farhan <farhan_75230@hotmail.com>
2025-07-19 21:11:51 +0200

    Books pages created

--- Frontend/src/app/(dashboard)/Books/create/page.tsx
+++ Frontend/src/app/(dashboard)/Books/create/page.tsx
@@ -1,15 +1,62 @@
 "use client";
 
 import React from "react";
+import { Button } from "@/components/ui/button";
 
 function page() {
   return (
     <div className="grid grid-cols-2 pl-8 pt-4">
-      <div>
-        <p>Edit Challenge</p>
+      <div className="mt-10">
+        <p>Book Picture</p>
       </div>
-      <div>
-        <p>Boom Details</p>
+      <div className="flex flex-col gap-10 mt-20">
+        {/* <form onSubmit={}> */}
+        <h1>Book Details</h1>
+        <label>
+          Title:
+          <br />
+          <input
+            id="name"
+            type="text"
+            name="username"
+            // value={name}
+            // onChange={handleNameChange}
+          />
+        </label>
+        {/* <br /> */}
+        {/* <br /> */}
+
+        <label>
+          Author:
+          <br />
+          <input
+            type="text"
+            name="profileUrl"
+            // value={email}
+            // onChange={handleEmailChange}
+          />
+        </label>
+        {/* <br /> */}
+        {/* <br /> */}
+
+        <label>
+          Genre:
+          <br />
+          <input
+            type="text"
+            name="fullProfileUrl"
+            // value={city}
+            // onChange={handleCityChange}
+          />
+        </label>
+        {/* <br /> */}
+        {/* <br /> */}
+        <div>
+          <Button type="submit" className="mr-2 bg-lime-600">
+            Create
+          </Button>
+        </div>
+        {/* </form> */}
       </div>
     </div>
   );
