4d20a9b1fc2e5c3f60d8ce4f16ac5d18bb463aea
Muhammad Farhan <farhan_75230@hotmail.com>
2025-07-19 22:26:53 +0200

    Minor changes in components

--- Frontend/src/components/sidebar.tsx
+++ Frontend/src/components/sidebar.tsx
@@ -6,15 +6,15 @@ function Sidebar() {
   return (
     <div className=" py-4 w-full h-screen max-h-screen min-h-[500px] min-w-[190px] col-span-1 bg-emerald-500">
       <div className="flex flex-col ml-2 w-20 gap-10 mt-1">
-        <Button className="bg-lime-600">
-          <Link href="/Books"> Books </Link>
-        </Button>
-        <Button className="bg-lime-600">
-          <Link href="/Offers">Offers</Link>
-        </Button>
-        <Button className="bg-lime-600">
-          <Link href="/Profile">Profile</Link>
-        </Button>
+        <Link href="/Books">
+          <Button className="bg-lime-600">Books</Button>
+        </Link>
+        <Link href="/Offers">
+          <Button className="bg-lime-600">Offers</Button>
+        </Link>
+        <Link href="/Profile">
+          <Button className="bg-lime-600">Profile</Button>
+        </Link>
       </div>
     </div>
   );
