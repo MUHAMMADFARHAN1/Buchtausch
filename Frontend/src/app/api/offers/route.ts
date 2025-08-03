8197b6427f0d3cfc94716256b3219c155009779b
Muhammad Farhan <farhan_75230@hotmail.com>
2025-08-03 19:55:54 +0200

    GET offer route generated

---
+++ Frontend/src/app/api/offers/route.ts
@@ -0,0 +1,22 @@
+// // app/api/data/route.ts
+import { NextResponse, NextRequest } from "next/server";
+import { auth, signIn, signOut } from "@/auth";
+
+export async function GET() {
+  let session: any = await auth();
+  console.log(session.accessToken);
+  let accessToken = session.accessToken;
+
+  const response = await fetch("http://127.0.0.1:5001/MyOffers", {
+    method: "GET",
+    headers: {
+      Authorization: accessToken,
+      //  Accept: "application/json",
+    },
+    mode: "cors",
+  });
+
+  const array = await response.json();
+  console.log(array);
+  return NextResponse.json(array);
+}
