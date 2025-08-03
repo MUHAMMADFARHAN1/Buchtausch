a74e974099ee0ceb06f61458ef5458c28584efa7
Muhammad Farhan <farhan_75230@hotmail.com>
2025-08-03 20:12:05 +0200

    Offer fetch from backend performed

--- Frontend/src/app/(dashboard)/Offers/page.tsx
+++ Frontend/src/app/(dashboard)/Offers/page.tsx
@@ -1,11 +1,54 @@
+"use client";
+
 import React from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Card from "@/components/MofferCard";
+import { useEffect, useState } from "react";
 
 //https://nextjs.org/docs/app/guides/migrating/app-router-migration#step-6-migrating-data-fetching-methods
 
-export default async function page() {
+function page() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true); // Initially true, so "loading" screen shows
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/offers")
+      .then((res) => {
+        if (!res.ok) throw new Error("Fetch failed");
+        return res.json();
+      })
+      .then((jsonData) => {
+        // let listItems = jsonData.map((item: any) => (
+        //   <Link href={`/Books/${item._id}`}>
+        //     <Card
+        //       name={item.title}
+        //       author={item.author}
+        //       genre={item.genre}
+        //       button={"check"}
+        //     />
+        //   </Link>
+        // ));
+        // setData(listItems);
+        setLoading(false); // Fetch done, so set loading to false
+        console.log(jsonData);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    // Render this while loading (this blocks showing the rest)
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <>
       <div className="flex flex-col justify-between mx-8 pt-2">
@@ -27,6 +70,7 @@ export default async function page() {
   );
 }
 
+export default page;
 // {
 //   projects.map((project) => <li key={project.id}>{project.name}</li>);
 // }
