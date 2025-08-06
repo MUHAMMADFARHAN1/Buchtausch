26d609650f4bf16cf6a65f505613e77e55f7b4ad
Muhammad Farhan <farhan_75230@hotmail.com>
2025-08-06 22:36:39 +0200

    Path updated

--- Frontend/src/app/(dashboard)/Offers/allinterests/details/[slug]/page.tsx
+++ Frontend/src/app/(dashboard)/Offers/allinterests/details/[slug]/page.tsx
@@ -1,29 +1,131 @@
+"use client";
+
 import React from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Card from "@/components/InterestCard";
 
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+
 //https://nextjs.org/docs/app/guides/migrating/app-router-migration#step-6-migrating-data-fetching-methods
 
-export default async function page() {
+function page() {
+  const params = useParams();
+  const slug = params.slug; // returns '123' from /profile/123
+  // console.log(slug);
+  const router = useRouter();
+
+  const [title, setTitle] = useState("empty");
+  const [description, setDescription] = useState("empty");
+  const [book, setBook] = useState("empty");
+
+  // const router = useRouter();
+
+  const [loading, setLoading] = useState(true); // Initially true, so "loading" screen shows
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/offers/" + slug)
+      .then((res) => {
+        if (!res.ok) throw new Error("Fetch failed");
+        return res.json();
+      })
+      .then((jsonData) => {
+        setLoading(false); // Fetch done, so set loading to false
+        console.log(jsonData);
+        setTitle(jsonData.title);
+        setDescription(jsonData.description);
+        setBook(jsonData.book.title);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
+  const Delete_Offer = () => {
+    fetch("http://localhost:3000/api/MyOffers/" + slug, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      // body: JSON.stringify({ title, author, }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Response:", data);
+      });
+    router.push("/Books");
+  };
+
+  if (loading) {
+    // Render this while loading (this blocks showing the rest)
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
-    <>
-      <div className="flex flex-col justify-between mx-8 pt-2">
-        <p>The interst can be accepted here</p>
-        {/* <div className="flex flex-row justify-between pt-2">
-          <Button className=" bg-lime-600 rounded">All (not my) Offers</Button>
-          <div className="flex flex-row justify-between gap-2">
-            <Link href={`/Offers/create`}>
-              <Button className=" bg-lime-600 rounded">Create Offer</Button>
-            </Link>
-            <Button className=" bg-lime-600 rounded">My Offers</Button>
-          </div>
-        </div> */}
-        <div className="flex flex-col justify-between pt-8">
-          <Card offer="Offer id" interest="Show Interest"></Card>
-          <Card offer="Offer id" interest="All Interests"></Card>
-        </div>
+    <div className="grid grid-cols-2 pl-8 pt-4">
+      <div className="mt-10">
+        <p>Offer</p>
       </div>
-    </>
+      <div className="flex flex-col gap-10 mt-20">
+        {/* <form onSubmit={}> */}
+        <h1>Offer Details</h1>
+        <label>
+          Title:
+          <br />
+          <input
+            id="name"
+            type="text"
+            name="username"
+            value={title}
+            readOnly
+            // onChange={handleNameChange}
+          />
+        </label>
+        {/* <br /> */}
+        {/* <br /> */}
+
+        <label>
+          Description:
+          <br />
+          <input
+            type="text"
+            name="profileUrl"
+            value={description}
+            readOnly
+            // onChange={handleEmailChange}
+          />
+        </label>
+        {/* <br /> */}
+        {/* <br /> */}
+
+        <label>
+          Book:
+          <br />
+          <input
+            type="text"
+            name="fullProfileUrl"
+            value={book}
+            readOnly
+            // onChange={handleCityChange}
+          />
+        </label>
+        {/* <br /> */}
+        {/* <br /> */}
+        <div>
+          <Button onClick={Delete_Offer} className="mr-2 bg-lime-600">
+            Accept Interest
+          </Button>
+        </div>
+        {/* </form> */}
+      </div>
+    </div>
   );
 }
+
+export default page;
