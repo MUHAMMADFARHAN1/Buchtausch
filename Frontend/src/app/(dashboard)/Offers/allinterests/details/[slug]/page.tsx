09c42323e4c560f1b3aee77a7152cfc567ef6e01
Muhammad Farhan <farhan_75230@hotmail.com>
2025-08-23 21:21:54 +0200

    Interest accept logic created

--- Frontend/src/app/(dashboard)/Offers/allinterests/details/[slug]/page.tsx
+++ Frontend/src/app/(dashboard)/Offers/allinterests/details/[slug]/page.tsx
@@ -8,12 +8,13 @@ import Card from "@/components/InterestCard";
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import { useRouter } from "next/navigation";
+import { deleteInterest, getInterestDetails } from "@/app/actions/interest";
 
 //https://nextjs.org/docs/app/guides/migrating/app-router-migration#step-6-migrating-data-fetching-methods
 
 function page() {
   const params = useParams();
-  const slug = params.slug; // returns '123' from /profile/123
+  const slug: any = params.slug; // returns '123' from /profile/123
   // console.log(slug);
   const router = useRouter();
 
@@ -27,33 +28,45 @@ function page() {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("http://localhost:3000/api/interests/details/" + slug)
-      .then((res) => {
-        if (!res.ok) throw new Error("Fetch failed");
-        return res.json();
-      })
-      .then((jsonData) => {
-        setLoading(false); // Fetch done, so set loading to false
+    //   fetch("http://localhost:3000/api/interests/details/" + slug)
+    //     .then((res) => {
+    //       if (!res.ok) throw new Error("Fetch failed");
+    //       return res.json();
+    //     })
+    //     .then((jsonData) => {
+    //       setLoading(false); // Fetch done, so set loading to false
+    //       console.log(jsonData);
+    //       setTitle(jsonData.title);
+    //       setDescription(jsonData.description);
+    //       setBook(jsonData.book.title);
+    //     })
+    //     .catch((err) => {
+    //       setError(err.message);
+    //       setLoading(false);
+    //     });
+    // }
+
+    getInterestDetails(slug)
+      .then((jsonData: any) => {
+        setLoading(false);
         console.log(jsonData);
         setTitle(jsonData.title);
         setDescription(jsonData.description);
         setBook(jsonData.book.title);
       })
-      .catch((err) => {
+      .catch((err: any) => {
         setError(err.message);
         setLoading(false);
       });
   }, []);
 
   const Delete_Offer = () => {
-    fetch("http://localhost:3000/api/interests/accept/" + slug, {
-      method: "DELETE",
-      headers: { "Content-Type": "application/json" },
-      // body: JSON.stringify({ title, author, }),
-    })
-      .then((res) => res.json())
+    deleteInterest(slug)
       .then((data) => {
         console.log("Response:", data);
+      })
+      .catch((err) => {
+        console.error("Delete error:", err.message);
       });
     router.push("/Books");
   };
